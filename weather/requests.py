6b2d0148dd53b9f09ebbe74d3cfc4711ee07a8fa
Ong, John Benedict <junnisan123@gmail.com>
2024-03-18 14:02:37 +0800

    summary

--- weather/requests.py
+++ weather/requests.py
@@ -27,7 +27,7 @@ def forecast_weather(request, city_name='Makati'):
         'q': city_name,
         'days':3,
     }
-    response = requests.get('http://api.weatherapi.com/v1/current.json', params=payload)
+    response = requests.get('http://api.weatherapi.com/v1/forecast.json', params=payload)
     
     print(response.status_code)
     print(response.url)
